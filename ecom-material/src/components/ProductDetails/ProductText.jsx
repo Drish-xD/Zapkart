e89b37b8c43b732745db61441bd9387334a2b2bf
Drish-xD <drish.xd@gmail.com>
2022-02-14 18:47:16 +0530

    added variants

--- ecom-material/src/components/ProductDetails/ProductText.jsx
+++ ecom-material/src/components/ProductDetails/ProductText.jsx
@@ -1,7 +1,17 @@
-import { Button, Paper, Typography } from "@mui/material";
+import { Button, Paper, Typography, Box } from "@mui/material";
+import CircleIcon from "@mui/icons-material/Circle";
 import React from "react";
 
 const ProductText = ({ product, onAddToCart }) => {
+  console.log(product.variant_groups);
+  const variantGroup = (variantgrp) => {
+    return variantgrp.options.map((variant) => (
+      <Button color="inherit" key={variant.name}>
+        <CircleIcon sx={{ color: `#${variant.name}`, border: "1px solid #000", borderRadius: "50%" }} />
+      </Button>
+    ));
+  };
+
   return (
     <>
       <Paper style={{ padding: "1rem" }} variant="outlined" square>
@@ -9,7 +19,11 @@ const ProductText = ({ product, onAddToCart }) => {
           {product.name}
         </Typography>
         <Typography variant="body1" dangerouslySetInnerHTML={{ __html: product.description }} />
-        <Typography variant="h5" component="h5" fontWeight="500">
+        <Box>
+          <Typography variant="h6">Choose Color</Typography>
+          {product.variant_groups.map((variantgrp) => variantGroup(variantgrp))}
+        </Box>
+        <Typography variant="h5" component="h5" fontWeight="500" mt={2}>
           {product.price.formatted_with_symbol}
         </Typography>
         <Button variant="contained" disableElevation sx={{ mt: 3 }} onClick={() => onAddToCart(product.id, 1)}>
