b7ea7a2c05f8ce07d4bcfdd961e7e0570c8ab566
Drish-xD <drish.xd@gmail.com>
2022-02-14 18:55:51 +0530

    fix navbar

--- ecom-material/src/components/Navbar/Navbar.jsx
+++ ecom-material/src/components/Navbar/Navbar.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Toolbar, AppBar, IconButton, Badge } from "@mui/material";
 import LocalMallRoundedIcon from "@mui/icons-material/LocalMallRounded";
 import AppleIcon from "@mui/icons-material/Apple";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Navbar = ({ totalitems }) => {
+  const { pathname } = useLocation();
   return (
     <>
       <AppBar position="fixed" color="inherit">
@@ -12,11 +13,13 @@ const Navbar = ({ totalitems }) => {
           <IconButton LinkComponent={NavLink} to="/" color="inherit" aria-label="Apple">
             <AppleIcon fontSize="large" />
           </IconButton>
-          <IconButton LinkComponent={NavLink} to="/cart" color="inherit" aria-label="Cart">
-            <Badge badgeContent={totalitems} color="primary">
-              <LocalMallRoundedIcon />
-            </Badge>
-          </IconButton>
+          {pathname !== "/cart" && (
+            <IconButton LinkComponent={NavLink} to="/cart" color="inherit" aria-label="Cart">
+              <Badge badgeContent={totalitems} color="primary">
+                <LocalMallRoundedIcon />
+              </Badge>
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
     </>
