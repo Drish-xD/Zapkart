f70977686cc853d098152bc76805789a6a504a2f
Drish-xD <drish.xd@gmail.com>
2022-02-20 17:08:55 +0530

    added redux

--- ecom-sementic/src/App.js
+++ ecom-sementic/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar, Footer } from "./components";
+import { Home, Cart, ShowProducts, ShowProduct, Checkout } from "./screens";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" index element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/Products" element={<ShowProducts />} />
+          <Route path="/Products/:id" element={<ShowProduct />} />
+          <Route path="/cart/checkout" element={<Checkout />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
--- ecom-sementic/src/index.js
+++ ecom-sementic/src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "semantic-ui-css/semantic.min.css";
-import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import App from "./App";
-import reducer from "./reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
