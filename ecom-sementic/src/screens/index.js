9643d045763883f693a360c0ce79a2b4cde0793d
Drish-xD <drish.xd@gmail.com>
2023-05-26 18:25:32 +0530

    change file structure

--- ecom-sementic/src/screens/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Cart } from "./Cart";
-export { default as Checkout } from "./Checkout";
-export { default as ShowProducts } from "./ShowProducts";
-export { default as ShowProduct } from "./ShowProduct";
